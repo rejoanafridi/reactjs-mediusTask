f33504471fe38fbaa320daaf414b7db1daa5dc77
rejoanafridi <rejoanafridi93@gmail.com>
2023-04-25 18:14:43 +0600

    problem 2 solved

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -7,6 +7,7 @@ const Problem2 = () => {
 	const [contacts, setContacts] = useState([]);
 	const [modalState, setModalState] = useState();
 	const [showModal, setShowModal] = useState(false);
+	const [searchText, setSearchText] = useState("");
 
 	const handleShow = (e) => {
 		setShowModal(true);
@@ -24,6 +25,19 @@ const Problem2 = () => {
 	};
 
 	const handleClose = () => setShowModal(false);
+
+	// search contact hendelere
+	const handleSearch = (e) => {
+		e.preventDefault();
+		const searchContact = async () => {
+			const result = await axios.get(
+				`https://contact.mediusware.com/api/contacts/?search=${searchText}`
+			);
+
+			setContacts(result.data.results);
+		};
+		searchContact();
+	};
 
 	const fetchContactData = async () => {
 		const result = await axios.get(
@@ -44,16 +58,18 @@ const Problem2 = () => {
 					<button
 						value="A"
 						onClick={(e) => handleShow(e)}
-						className="btn btn-lg btn-outline-primary"
+						className="btn btn-lg btn-outline-primary text-white"
 						type="button"
+						style={{ backgroundColor: "#46139f" }}
 					>
 						All Contacts
 					</button>
 					<button
 						value="B"
 						onClick={(e) => handleShow(e)}
-						className="btn btn-lg btn-outline-warning"
+						className="btn btn-lg btn-outline-warning text-white"
 						type="button"
+						style={{ backgroundColor: "#ff7f50" }}
 					>
 						US Contacts
 					</button>
@@ -64,26 +80,48 @@ const Problem2 = () => {
 					</Modal.Header>
 
 					<Modal.Body>
-						<div className="d-flex justify-content-center gap-3">
+						<div className="search-container">
+							<div className="input-group">
+								<div className="form-outline w-full">
+									<form onSubmit={handleSearch} className="d-flex">
+										<input
+											type="search"
+											id="form1"
+											placeholder="Search"
+											className="form-control"
+											onChange={(e) => setSearchText(e.target.value)}
+										/>
+										<button type="submit">submit</button>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div className="d-flex justify-content-center gap-3 mt-5">
 							<button
 								value="A"
 								onClick={(e) => handleShow(e)}
-								className="btn btn-lg btn-outline-primary"
+								className="btn btn-lg btn-outline-primary text-white"
 								type="button"
+								style={{ backgroundColor: "#46139f" }}
 							>
 								All Contacts
 							</button>
 							<button
 								value="B"
 								onClick={(e) => handleShow(e)}
-								className="btn btn-lg btn-outline-warning"
+								className="btn btn-lg btn-outline-warning text-white"
 								type="button"
+								style={{ backgroundColor: "#ff7f50" }}
 							>
 								US Contacts
 							</button>
 							<button
-								className="btn btn-lg btn-outline-primary"
+								className="btn btn-lg  text-primary"
 								onClick={handleClose}
+								style={{
+									backgroundColor: "#ffff",
+									border: "2px solid #46139f",
+								}}
 							>
 								Close
 							</button>
