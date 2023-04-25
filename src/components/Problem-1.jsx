ce85374e5d304409ac547371d1d99aa90f18e1f3
rejoanafridi <rejoanafridi93@gmail.com>
2023-04-25 15:43:41 +0600

    problem one solved

---
+++ src/components/Problem-1.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+
+const Problem1 = () => {
+	const [show, setShow] = useState("all");
+	const [formData, setFormData] = useState([]);
+	const [inputForm, setInputForm] = useState({
+		name: "",
+		status: "",
+	});
+
+	// filter function
+	const filterData = formData.filter((item) => {
+		if (show === "all") {
+			return item;
+		} else if (show == "active") {
+			return item.status === show;
+		} else {
+			return item.status === show;
+		}
+	});
+
+	const handleClick = (val) => {
+		setShow(val);
+	};
+	console.log(show);
+	// handle form submit
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setFormData([...formData, inputForm]);
+		setInputForm({
+			name: "",
+			status: "",
+		});
+	};
+
+	return (
+		<div className="container">
+			<div className="row justify-content-center mt-5">
+				<h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+				<div className="col-6 ">
+					<form
+						className="row gy-2 gx-3 align-items-center mb-4"
+						onSubmit={handleSubmit}
+					>
+						<div className="col-auto">
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Name"
+								value={inputForm.name}
+								onChange={(e) =>
+									setInputForm({ ...inputForm, name: e.target.value })
+								}
+							/>
+						</div>
+						<div className="col-auto">
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Status"
+								value={inputForm.status}
+								onChange={(e) =>
+									setInputForm({ ...inputForm, status: e.target.value })
+								}
+							/>
+						</div>
+						<div className="col-auto">
+							<button type="submit" className="btn btn-primary">
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+				<div className="col-8">
+					<ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+						<li className="nav-item">
+							<button
+								className={`nav-link ${show === "all" && "active"}`}
+								type="button"
+								onClick={() => handleClick("all")}
+							>
+								All
+							</button>
+						</li>
+						<li className="nav-item">
+							<button
+								className={`nav-link ${show === "active" && "active"}`}
+								type="button"
+								onClick={() => handleClick("active")}
+							>
+								Active
+							</button>
+						</li>
+						<li className="nav-item">
+							<button
+								className={`nav-link ${show === "completed" && "active"}`}
+								type="button"
+								onClick={() => handleClick("completed")}
+							>
+								Completed
+							</button>
+						</li>
+					</ul>
+					<div className="tab-content"></div>
+					<table className="table table-striped ">
+						<thead>
+							<tr>
+								<th scope="col">Name</th>
+								<th scope="col">Status</th>
+							</tr>
+						</thead>
+						{filterData.map((data, id) => (
+							<React.Fragment key={id}>
+								<tbody>
+									<td>{data.name}</td>
+									<td>{data.status}</td>
+								</tbody>
+							</React.Fragment>
+						))}
+					</table>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Problem1;
